1a5f955e03ff180727e84937b80e695fbfc36420
marianacastanedar <167562546+marianacastanedar@users.noreply.github.com>
2025-01-30 18:02:44 -0600

    metodos de la interfaz, push pop y operation

--- CalculatorADT.java
+++ CalculatorADT.java
@@ -1,6 +1,38 @@
 import java.util.Stack;
 
-public class CalculatorADT implements IStack<Integer>{
-    
+public class CalculatorADT implements IStack<Integer> {
+    private Stack<Integer> stack;
+
+    public CalculatorADT() {
+        stack = new Stack<>();
+    }
+
+    @Override
+    public void push(Integer value) {
+        stack.push(value);
+    }
+
+    @Override
+    public Integer pop() {
+        if (stack.isEmpty()) {
+            throw new IllegalStateException("La pila está vacía");
+        }
+        return stack.pop();
+    }
+
+    @Override
+    public Integer operation(char operator, Integer value1, Integer value2) {
+        switch (operator) {
+            case '+': return value1 + value2;
+            case '-': return value1 - value2;
+            case '*': return value1 * value2;
+            case '/': 
+                if (value2 == 0) throw new ArithmeticException("División por cero");
+                return value1 / value2;
+            default: throw new IllegalArgumentException("Operador no soportado: " + operator);
+        }
+    }
+
+
 
 }
