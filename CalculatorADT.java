89a7828eb071b946bd48af569db93efae3ad7001
marianacastanedar <167562546+marianacastanedar@users.noreply.github.com>
2025-01-30 18:57:44 -0600

    cambios para finalizar los metodos de calculaator

--- CalculatorADT.java
+++ CalculatorADT.java
@@ -12,6 +12,7 @@ public class CalculatorADT implements IStack<Integer> {
         stack.push(value);
     }
 
+
     @Override
     public Integer pop() {
         if (stack.isEmpty()) {
@@ -20,19 +21,40 @@ public class CalculatorADT implements IStack<Integer> {
         return stack.pop();
     }
 
+    //method to recognize the operation 
     @Override
     public Integer operation(char operator, Integer value1, Integer value2) {
         switch (operator) {
-            case '+': return value1 + value2;
-            case '-': return value1 - value2;
-            case '*': return value1 * value2;
-            case '/': 
-                if (value2 == 0) throw new ArithmeticException("División por cero");
+            case '+':
+                return value1 + value2;
+            case '-':
+                return value1 - value2;
+            case '*':
+                return value1 * value2;
+            case '/':
+                if (value2 == 0)
+                    throw new ArithmeticException("División por cero");
                 return value1 / value2;
-            default: throw new IllegalArgumentException("Operador no soportado: " + operator);
+            default:
+                throw new IllegalArgumentException("Operador no soportado: " + operator);
         }
     }
+    
+        //Method to consider postfix 
+        public int evaluatePostfix(String expression) {
+            for (char c : expression.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    push(Character.getNumericValue(c));
+                } else {
+                    int value2 = pop();
+                    int value1 = pop();
+                    int result = operation(c, value1, value2);
+                    push(result);
+                }
+            }
+            return pop();
 
+        }
 
 
 }
